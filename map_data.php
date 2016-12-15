00868377b812fc79806382f26d45bdc480b34bfe
pratsoni <pratsoni>
2016-12-15 22:44:24 +0530

    First commit

---
+++ map_data.php
@@ -0,0 +1,99 @@
+<?php
+$data='{
+  "total_records": 13,
+  "message": "Data fetched successfully",
+  "Data": [
+    {
+      "id": "1231412341234",
+      "gatewaymeta": {
+        "latitude": 24.050527,
+        "longitude": 70.498536,
+        "altitude": 30.3,
+        "provider": "Airtel"
+      }
+	},
+	{
+      "id": "1231412341234",
+      "gatewaymeta": {
+        "latitude": 24.050527,
+        "longitude": 70.498536,
+        "altitude": 30.3,
+        "provider": "Airtel"
+      }
+	},
+	{
+      "id": "13241234",
+      "gatewaymeta": {
+        "latitude": 25.050527,
+        "longitude": 24.498536,
+        "altitude": 30.3,
+        "provider": "Airtel"
+      }
+	},
+	{
+      "id": "13241234",
+      "gatewaymeta": {
+        "latitude": 25.050527,
+        "longitude": 24.498536,
+        "altitude": 30.3,
+        "provider": "Airtel"
+      }
+	},
+	{
+      "id": "13241234",
+      "gatewaymeta": {
+        "latitude": 25.050527,
+        "longitude": 24.498536,
+        "altitude": 30.3,
+        "provider": "Airtel"
+      }
+	},
+	{
+      "id": "13241234",
+      "gatewaymeta": {
+        "latitude": 25.050527,
+        "longitude": 24.498536,
+        "altitude": 30.3,
+        "provider": "Airtel"
+      }
+	},
+	{
+      "id": "13241234",
+      "gatewaymeta": {
+        "latitude": 25.050527,
+        "longitude": 24.498536,
+        "altitude": 30.3,
+        "provider": "Airtel"
+      }
+	},
+	{
+      "id": "1231412341234",
+      "gatewaymeta": {
+        "latitude": 20.050527,
+        "longitude": 50.498536,
+        "altitude": 30.3,
+        "provider": "Airtel"
+      }
+	},
+	{
+      "id": "12314123412342134234",
+      "gatewaymeta": {
+        "latitude": 29.050527,
+        "longitude": 55.498536,
+        "altitude": 30.3,
+        "provider": "Airtel"
+      }
+	},
+	{
+      "id": "1231412341234",
+      "gatewaymeta": {
+        "latitude": 32.050527,
+        "longitude": 51.498536,
+        "altitude": 30.3,
+        "provider": "Airtel"
+      }
+	}
+	
+]}';
+echo $data;
+?>
